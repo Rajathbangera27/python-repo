43c864ed7471b6de8b79d93ad863798c35917c86
RajathBangera <rajathbangera6023@gmail.com>
2026-03-22 13:10:36 +0530

    Added two sum and unique pgm

---
+++ Guess_No/main.py
@@ -0,0 +1,17 @@
+import random
+
+class GuessANumber:
+    
+    def play_game(self):
+        system_number = random.randint(1, 10)
+        
+        user_guess = int(input("Guess a number between 1 and 10: "))
+        
+        if user_guess == system_number:
+            print("User guess is right")
+        else:
+            print("User guess is wrong")
+            print("The correct number was:", system_number)
+
+game = GuessANumber()
+game.play_game()
---
+++ Save_content/main.py
@@ -0,0 +1,14 @@
+import os
+
+filename = input("Enter the file name: ")
+
+if os.path.exists(filename):
+    print("File already found")
+else:
+    content = input("Enter the content to write into the file: ")
+    
+    file = open(filename, "w")
+    file.write(content)
+    file.close()
+    
+    print("File created and content written successfully")
